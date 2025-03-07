34356d5d03729842430fc716df1c49e78240679a
mindula-madhuhansa <mimmindula@gmail.com>
2025-03-07 13:22:58 +0530

    Set default payment method to cash in checkout form

--- components/checkout-form.tsx
+++ components/checkout-form.tsx
@@ -205,6 +205,7 @@ export default function CheckoutForm() {
             />
 
             <FormField
+              defaultValue="cash"
               control={form.control}
               name="paymentMethod"
               render={({ field }) => (
